7e185ba8ff6174b620f0e38ec2cbea829f49bedb
IvambergSilva <ivambergisilva@gmail.com>
2024-05-02 12:55:34 -0300

    feat: add banners on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,27 @@
 import Header from "./_components/header";
 import Search from "./_components/search";
 import CategoryList from "./_components/categoryList";
+import Image from "next/image";
 
 export default function Home() {
     return (
         <div>
-            <div className="px-5 pt-6">
+            <div className="px-5 py-6">
                 <Header />
             </div>
-            <div className="px-5 mt-6">
+            <div className="px-5">
                 <Search />
             </div>
-            <div className="pt-6 pb-6">
+            <div className="py-6">
                 <CategoryList />
+            </div>
+
+            <div className="px-5">
+                <Image src="/banner-01.png" alt="Até 30% de esconto em pizzas!" width={0} height={0} className="h-auto w-full object-contain" quality={100} sizes="100vw" />
+            </div>
+
+            <div className="py-6 px-5">
+                <Image src="/banner-02.png" alt="A partir de R$ 17,90 em lanches" width={0} height={0} className="h-auto w-full object-contain" quality={100} sizes="100vw" />
             </div>
         </div>
     );
