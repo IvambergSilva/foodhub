81c9faa31f5850dba4ee44bedc76ff0da5cd652b
IvambergSilva <ivambergisilva@gmail.com>
2024-05-03 22:49:44 -0300

    feat: add the sectionTitle component

---
+++ app/_components/sectionTitle.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Button } from './ui/button'
+import Link from 'next/link'
+import { ChevronRightIcon } from 'lucide-react'
+
+interface SectionTitleProps {
+    title: string
+}
+
+export default function SectionTitle({ title }: SectionTitleProps) {
+    return (
+        <div className="flex items-center justify-between mt-6 px-5">
+            <h2 className="text-base font-semibold">{title}</h2>
+            <Button
+                variant="ghost"
+                className="h-fit p-0 text-primary hover:bg-transparent"
+                asChild
+            >
+                <Link href="">
+                    <span>Ver todos</span>
+                    <ChevronRightIcon size={16} />
+                </Link>
+            </Button>
+        </div>
+    )
+}
