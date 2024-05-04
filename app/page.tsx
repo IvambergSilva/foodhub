a440c6698de989bf4f7c4b43356f9f0b5f864882
IvambergSilva <ivambergisilva@gmail.com>
2024-05-03 23:05:30 -0300

    feat: add additional components to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,32 @@
 import Header from "./_components/header";
 import Search from "./_components/search";
 import CategoryList from "./_components/categoryList";
-import Image from "next/image";
+import ProductList from "./_components/productList";
+import PromoBanner from "./_components/promoBanner";
+import { db } from "./_lib/prisma";
+import RestaurantList from "./_components/restaurantList";
+import { Button } from "./_components/ui/button";
+import Link from "next/link";
+import { ChevronRightIcon } from "lucide-react";
 
-export default function Home() {
+export default async function Home() {
+    const products = await db.product.findMany({
+        where: {
+            discountPercentage: {
+                gt: 0
+            },
+            restaurant: {
+                deliveryFee: 0
+            }
+        },
+        take: 10,
+        include: {
+            restaurant: true
+        }
+    })
+
     return (
-        <div>
+        <>
             <div className="px-5 py-6">
                 <Header />
             </div>
@@ -17,12 +38,41 @@ export default function Home() {
             </div>
 
             <div className="px-5">
-                <Image src="/banner-01.png" alt="Até 30% de esconto em pizzas!" width={0} height={0} className="h-auto w-full object-contain" quality={100} sizes="100vw" />
+                <PromoBanner
+                    src="/banner-01.png"
+                    alt="Até 30% de esconto em pizzas!"
+                />
             </div>
 
-            <div className="py-6 px-5">
-                <Image src="/banner-02.png" alt="A partir de R$ 17,90 em lanches" width={0} height={0} className="h-auto w-full object-contain" quality={100} sizes="100vw" />
+            <div className="py-6">
+                <ProductList products={products} />
             </div>
-        </div>
+
+            <div className="px-5">
+                <PromoBanner
+                    src="/banner-02.png"
+                    alt="A partir de R$ 17,90 em lanches"
+                />
+            </div>
+
+            <div className="space-y-4 py-6">
+                <div className="flex items-center justify-between px-5">
+                    <h2 className="text-base font-semibold">Restaurante recomendados</h2>
+
+                    <Button
+                        variant="ghost"
+                        className="h-fit p-0 text-primary hover:bg-transparent"
+                        asChild
+                    >
+                        <Link href="">
+                            <span>Ver todos</span>
+                            <ChevronRightIcon size={16} />
+                        </Link>
+                    </Button>
+                </div>
+
+                <RestaurantList />
+            </div>
+        </>
     );
 }
