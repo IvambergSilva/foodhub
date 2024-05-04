ed1c3a50c36dd7adedf5cddcf25d03b065d00c96
IvambergSilva <ivambergisilva@gmail.com>
2024-05-03 22:50:47 -0300

    feat: add the categories components

--- app/_components/categoryItem.tsx
+++ app/_components/categoryItem.tsx
@@ -1,18 +1,31 @@
+'use client'
+
+import React from "react";
 import Image from "next/image";
 
-interface CategoryItemProps {
+interface CategoryProps {
     id: string;
     name: string;
     imageUrl: string;
 }
 
-export default function CategoryItem({ id, name, imageUrl }: CategoryItemProps) {
+interface CategoryItemProps {
+    categories: CategoryProps[]
+}
+
+export default function CategoryItem({ categories }: CategoryItemProps) {
     return (
-        <div className="flex items-center gap-3 bg-white rounded-full p-4 min-w-fit shadow-[0_5px_5px_0px_#0000000F]">
-            <div className="relative w-[30px] h-[30px]">
-                <Image src={imageUrl} alt={name} fill />
-            </div>
-            <span className="font-semibold text-sm">{name}</span>
+        <div
+            className="flex items-center gap-3 w-full overflow-x-scroll px-4 pb-2 [&::-webkit-scrollbar]:hidden"
+        >
+            {categories.map((category) => (
+                <div className="flex items-center gap-3 bg-white rounded-full p-4 min-w-fit shadow-[0_5px_5px_0px_#0000000F]">
+                    <div className="relative w-[30px] h-[30px]">
+                        <Image src={category.imageUrl} alt={category.name} fill />
+                    </div>
+                    <span className="font-semibold text-sm">{category.name}</span>
+                </div>
+            ))}
         </div>
     )
 }
--- app/_components/categoryList.tsx
+++ app/_components/categoryList.tsx
@@ -1,53 +1,16 @@
-'use client'
-
-import React, { useEffect, useRef, useState } from "react";
-import { api } from "../_lib/api";
+import { db } from "../_lib/prisma";
 import CategoryItem from "./categoryItem";
 
-interface CategoriesProps {
-    id: string;
-    name: string;
-    imageUrl: string;
-}
-
-export default function CategoryList() {
-    const [categories, setCategories] = useState<CategoriesProps[]>([]);
-    const categoriesContainerRef = useRef<HTMLDivElement>(null)
-
-    const handleScroll = (e: React.WheelEvent<HTMLDivElement>) => {
-        const container = categoriesContainerRef.current;
-        if (container) {
-            container.scrollTo({
-                left: container.scrollLeft + e.deltaY,
-                behavior: "smooth",
-            });
+export default async function CategoryList() {
+    const categories = await db.category.findMany({
+        select: {
+            id: true,
+            name: true,
+            imageUrl: true
         }
-    }
-
-    useEffect(() => {
-        const url = `${api}/categories`
-        fetch(url)
-            .then(response => response.json()
-                .then(data => {
-                    console.log(data);
-                    setCategories(data.categories)
-                }))
-    }, [])
+    });
 
     return (
-        <div
-            ref={categoriesContainerRef}
-            onWheel={handleScroll}
-            className="flex items-center gap-3 w-full overflow-x-hidden px-4 pb-2"
-        >
-            {categories.map((category) => (
-                <CategoryItem
-                    key={category.id}
-                    id={category.id}
-                    name={category.name}
-                    imageUrl={category.imageUrl}
-                />
-            ))}
-        </div>
+        <CategoryItem categories={categories} />
     )
 }
