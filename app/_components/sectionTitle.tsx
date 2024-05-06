d942e251cd8651a632ef236b7aaf2f55fa358dd6
IvambergSilva <ivambergisilva@gmail.com>
2024-05-05 22:00:51 -0300

    feat: add navigation parameter to the section title component

--- app/_components/sectionTitle.tsx
+++ app/_components/sectionTitle.tsx
@@ -5,9 +5,10 @@ import { ChevronRightIcon } from 'lucide-react'
 
 interface SectionTitleProps {
     title: string
+    href: string
 }
 
-export default function SectionTitle({ title }: SectionTitleProps) {
+export default function SectionTitle({ title, href }: SectionTitleProps) {
     return (
         <div className="flex items-center justify-between mt-6 px-5">
             <h2 className="text-base font-semibold">{title}</h2>
@@ -16,7 +17,7 @@ export default function SectionTitle({ title }: SectionTitleProps) {
                 className="h-fit p-0 text-primary hover:bg-transparent"
                 asChild
             >
-                <Link href="">
+                <Link href={href}>
                     <span>Ver todos</span>
                     <ChevronRightIcon size={16} />
                 </Link>
