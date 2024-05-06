31a8dc7793c3259a01b3c0642303e78e2c7c2f53
IvambergSilva <ivambergisilva@gmail.com>
2024-05-05 22:12:41 -0300

    feat: enable search funcionality

--- app/_components/search.tsx
+++ app/_components/search.tsx
@@ -1,15 +1,38 @@
+'use client'
+
 import { SearchIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
+import React, { FormEvent, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Search() {
-    return (
-        <div className="flex items-center gap-2">
-            <Input placeholder="Buscar Restaurantes" className="border-none flex-1" />
+    const router = useRouter();
+    const [search, setSearch] = useState("");
 
-            <Button size="icon" className="w-10">
+    function handleSearch(e: React.ChangeEvent<HTMLInputElement>) {
+        setSearch(e.target.value)
+    }
+
+    function handleSearchSubmit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        if(!search) return
+
+        router.push(`/restaurants?name=${search}`)
+    };
+
+    return (
+        <form className="flex items-center gap-2" onSubmit={handleSearchSubmit}>
+            <Input
+                placeholder="Buscar Restaurantes"
+                className="border-none flex-1"
+                onChange={handleSearch}
+                value={search}
+            />
+            <Button size="icon" className="w-10" type="submit">
                 <SearchIcon size={20} />
             </Button>
-        </div>
+        </form>
     )
 }
