9e08dddafb47d0020191f3a192af6d99b1772f5d
IvambergSilva <ivambergisilva@gmail.com>
2024-05-05 22:04:42 -0300

    feat: add recommend orders

---
+++ app/orders/recommends/page.tsx
@@ -0,0 +1,25 @@
+import Header from "@/app/_components/header";
+import ProductItem from "@/app/_components/productItem";
+import { db } from "@/app/_lib/prisma";
+
+export default async function page() {
+    const products = await db.product.findMany({
+        take: 10,
+        include: {
+            restaurant: true
+        },
+    })
+
+    return (
+        <div className="px-5 w-full py-5">
+            <Header />
+
+            <h2 className="text-lg font-semibold py-6">Pedidos Recomendados</h2>
+            <div className="grid grid-cols-2 gap-y-3 gap-x-5">
+                {products.map((product) => (
+                    <ProductItem product={product} />
+                ))}
+            </div>
+        </div>
+    )
+}
