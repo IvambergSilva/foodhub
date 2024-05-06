910403e0b667960ccb8b02799631bbff5a6f3c33
IvambergSilva <ivambergisilva@gmail.com>
2024-05-05 22:11:51 -0300

    refactor: improve restaurant item component

--- app/_components/restaurantItem.tsx
+++ app/_components/restaurantItem.tsx
@@ -4,6 +4,7 @@ import Image from "next/image"
 import { formatCurrency } from "../_helpers/price"
 import Link from "next/link"
 import { Button } from "./ui/button"
+import RatingBadge from "./ratingBadge"
 
 interface RestaurantItemsProps {
     restaurant: Restaurant
@@ -12,19 +13,18 @@ interface RestaurantItemsProps {
 export default function RestaurantItem({ restaurant }: RestaurantItemsProps) {
     return (
         <Link href={`/restaurants/${restaurant.id}`}>
-            <div className="min-w-[266px] max-w-[266px]">
+            <div className="min-w-[266px]">
                 <div className="relative h-[136px] mb-3">
                     <Image src={restaurant.imageUrl} alt={restaurant.name} fill className="object-cover rounded-lg" />
 
-                    <div className="absolute text-xs top-2 left-2 bg-primary flex items-center justify-center rounded-md px-2 py-0.5 gap-0.5 bg-white">
-                        <StarIcon size={12} className="fill-yellow-500 text-yellow-500" />
-                        <span className="text-xs font-semibold">{Number(restaurant.ratings)}</span>
+                    <div className="absolute top-2 left-2 bg-primary rounded-full bg-white">
+                        <RatingBadge restaurant={restaurant} />
                     </div>
 
                     <Button
                         variant="ghost"
                         size="icon"
-                        className="absolute right-2 top-2 w-7 h-7 bg-gray-700 hover:bg-transparent rounded-full"
+                        className="absolute right-2 top-2 w-7 h-7 rounded-full bg-gray-700 hover:bg-transparent"
                     >
                         <HeartIcon size={16} className="fill-white" />
                     </Button>
@@ -34,14 +34,18 @@ export default function RestaurantItem({ restaurant }: RestaurantItemsProps) {
 
                 <div className="flex items-center text-muted-foreground gap-4 text-xs">
                     <div className="flex items-center gap-1">
-                        <BikeIcon size={14} />
+                        <span className="text-primary">
+                            <BikeIcon size={14} />
+                        </span>
                         <span>{Number(restaurant.deliveryFee) === 0
                             ? 'Entrega Grátis'
                             : formatCurrency(Number(restaurant.deliveryFee))}
                         </span>
                     </div>
                     <div className="flex items-center gap-1">
-                        <TimerIcon size={14} />
+                        <span className="text-primary">
+                            <TimerIcon size={14} />
+                        </span>
                         <span>{restaurant.deliveryTimeMinutes} min</span>
                     </div>
                 </div>
